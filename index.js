d14bfb75fe0e47061efebb4a5a2bc09b3ef19780
Riya Kapoor <riya_kapoor@hotmail.com>
2024-11-22 02:52:10 +0530

    Initial commit consists of part 1 of extension- fetching & displaying all open tabs on popup as extn

---
+++ index.js
@@ -0,0 +1,26 @@
+const rr="hi"
+const dat=document.querySelector("#yes")
+dat.innerHTML=rr
+console.log("hhsicihsdic");
+
+chrome.windows.getAll({populate:true}, getAllOpenWindows);
+
+function getAllOpenWindows(winData) {
+  console.log(winData);
+  
+  var tabs = [];
+  for (var i in winData) {
+    // if (winData[i].focused === true) {
+        var winTabs = winData[i].tabs;
+        var totTabs = winTabs.length;
+        for (var j=0; j<totTabs;j++) {
+          tabs.push(winTabs[j].url);
+        }
+    // }
+  }
+  console.log(tabs);
+  const r="hiiii"
+  const data=document.querySelector("#yes")
+//   const data="hi"
+  data.innerHTML=tabs.length+" "+tabs[0]
+}
